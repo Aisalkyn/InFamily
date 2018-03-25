4a72b8f85cce0281faa1bf49dea59fc498f22211
Tentimischov <b2a2h2a2>
2018-03-25 12:24:52 +0600

    quiz is done

--- app/src/main/java/com/example/nestana/infamily/ui/quiz/QuizActivity.java
+++ app/src/main/java/com/example/nestana/infamily/ui/quiz/QuizActivity.java
@@ -3,13 +3,11 @@ package com.example.nestana.infamily.ui.quiz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
+
 
 import com.example.nestana.infamily.R;
 
