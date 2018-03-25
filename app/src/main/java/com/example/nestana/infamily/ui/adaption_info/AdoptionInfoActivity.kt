e953c74c80f0610ba9f0488cdba53ecfd5ffbdef
Aisalkyn <aisa.tashmatova@gmail.com>
2018-03-25 15:24:22 +0600

    Айсалкын не нервничай

--- app/src/main/java/com/example/nestana/infamily/ui/adaption_info/AdoptionInfoActivity.kt
+++ app/src/main/java/com/example/nestana/infamily/ui/adaption_info/AdoptionInfoActivity.kt
@@ -2,6 +2,7 @@ package com.example.nestana.infamily.ui.adaption_info
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.example.nestana.infamily.R
 import com.example.nestana.infamily.ui.adaption_info.adopt.AdoptFragment
 import com.example.nestana.infamily.ui.adaption_info.foster.FosterFragment
@@ -23,7 +24,14 @@ class AdoptionInfoActivity : AppCompatActivity(){
         adapter.addFragment(FosterFragment(), getString(R.string.foster))
         tabanim_viewpager.adapter = adapter
         tabanim_tabs.setupWithViewPager(tabanim_viewpager)
-
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item!!.itemId==android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
